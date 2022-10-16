1e855404d52c4f03bdac2ac21efb6b0fe2e66adc
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-10-16 14:51:42 +0530

    host

--- src/actions/pizzaActions.js
+++ src/actions/pizzaActions.js
@@ -1,89 +1,91 @@
-import axios from "axios";
-export const getAllPizzas=()=>async dispatch=>{
+import axios from 'axios';
 
-    dispatch({type:'GET_PIZZAS_REQUEST'})
+const config = require('../config.json');
 
-    try {
-        const response = await axios.get('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/getallpizzas')
-        console.log(response);
-        dispatch({type:'GET_PIZZAS_SUCCESS' , payload : response.data})
-       
-    } catch (error) {
-        dispatch({type:'GET_PIZZAS_FAILED' , payload : error})
-    }
+export const getAllPizzas = () => async (dispatch) => {
+  dispatch({ type: 'GET_PIZZAS_REQUEST' });
 
-}
-
-export const getPizzaById=(pizzaid)=>async dispatch=>{
-
-    dispatch({type:'GET_PIZZABYID_REQUEST'})
-
-    try {
-        const response = await axios.post('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/getpizzabyid' , {pizzaid})
-        console.log(response);
-        dispatch({type:'GET_PIZZABYID_SUCCESS' , payload : response.data})
-    } catch (error) {
-        dispatch({type:'GET_PIZZABYID_FAILED' , payload : error})
-    }
-
-}
-
-export const filterPizzas=(searchkey , category)=>async dispatch=>{
-
-  
-    dispatch({type:'GET_PIZZAS_REQUEST'})
-
-    try {
-        var filteredPizzas ;
-        const response = await axios.get('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/getallpizzas')
-        filteredPizzas = response.data.filter(pizza=>pizza.name.toLowerCase().includes(searchkey))
-         
-        if(category!='all')
-        {
-            filteredPizzas = response.data.filter(pizza=>pizza.category.toLowerCase()==category)
-
-        }
-        dispatch({type:'GET_PIZZAS_SUCCESS' , payload : filteredPizzas})
-    } catch (error) {
-        dispatch({type:'GET_PIZZAS_FAILED' , payload : error})
-    }
-
-}
-
-export const addPizza=(pizza)=>async dispatch=>{
-    dispatch({type:'ADD_PIZZA_REQUEST'})
-    try {
-        const response= await axios.post('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/addpizza' , {pizza})
-        console.log(response);
-        dispatch({type:'ADD_PIZZA_SUCCESS'})
-    } catch (error) {
-        dispatch({type:'ADD_PIZZA_FAILED' , payload : error})
-    }
-}
-
-export const editPizza=(editedpizza)=>async dispatch=>{
-    dispatch({type:'EDIT_PIZZA_REQUEST'})
-    try {
-        const response= await axios.post('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/editpizza' , {editedpizza})
-        console.log(response);
-        dispatch({type:'EDIT_PIZZA_SUCCESS'})
-        window.location.href='https://pizza-delivery-app-backend.herokuapp.com/admin/pizzaslist'
-    } catch (error) {
-        dispatch({type:'EDIT_PIZZA_FAILED' , payload : error})
-    }
-}
-
-export const deletePizza=(pizzaid)=>async dispatch=>{
-
-try {
-    const response =await axios.post('https://pizza-delivery-app-backend.herokuapp.com/api/pizzas/deletepizza' , {pizzaid})
-    alert('Pizza Deleted Successfully')
+  try {
+    const response = await axios.get(`${config.HOST}/pizzas/getallpizzas`);
     console.log(response);
-    window.location.reload()
-} catch (error) {
-    alert('Something went wrong')
-    console.log(error);
-}
-       
+    dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'GET_PIZZAS_FAILED', payload: error });
+  }
+};
 
-}
+export const getPizzaById = (pizzaid) => async (dispatch) => {
+  dispatch({ type: 'GET_PIZZABYID_REQUEST' });
+
+  try {
+    const response = await axios.post(`${config.HOST}/pizzas/getpizzabyid`, {
+      pizzaid,
+    });
+    console.log(response);
+    dispatch({ type: 'GET_PIZZABYID_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'GET_PIZZABYID_FAILED', payload: error });
+  }
+};
+
+export const filterPizzas = (searchkey, category) => async (dispatch) => {
+  dispatch({ type: 'GET_PIZZAS_REQUEST' });
+
+  try {
+    var filteredPizzas;
+    const response = await axios.get(`${config.HOST}/pizzas/getallpizzas`);
+    filteredPizzas = response.data.filter((pizza) =>
+      pizza.name.toLowerCase().includes(searchkey)
+    );
+
+    if (category != 'all') {
+      filteredPizzas = response.data.filter(
+        (pizza) => pizza.category.toLowerCase() == category
+      );
+    }
+    dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: filteredPizzas });
+  } catch (error) {
+    dispatch({ type: 'GET_PIZZAS_FAILED', payload: error });
+  }
+};
+
+export const addPizza = (pizza) => async (dispatch) => {
+  dispatch({ type: 'ADD_PIZZA_REQUEST' });
+  try {
+    const response = await axios.post(`${config.HOST}/pizzas/addpizza`, {
+      pizza,
+    });
+    console.log(response);
+    dispatch({ type: 'ADD_PIZZA_SUCCESS' });
+  } catch (error) {
+    dispatch({ type: 'ADD_PIZZA_FAILED', payload: error });
+  }
+};
+
+export const editPizza = (editedpizza) => async (dispatch) => {
+  dispatch({ type: 'EDIT_PIZZA_REQUEST' });
+  try {
+    const response = await axios.post(`${config.HOST}/pizzas/editpizza`, {
+      editedpizza,
+    });
+    console.log(response);
+    dispatch({ type: 'EDIT_PIZZA_SUCCESS' });
+    window.location.href = `${config.HOST}/admin/pizzaslist`;
+  } catch (error) {
+    dispatch({ type: 'EDIT_PIZZA_FAILED', payload: error });
+  }
+};
+
+export const deletePizza = (pizzaid) => async (dispatch) => {
+  try {
+    const response = await axios.post(`${config.HOST}/pizzas/deletepizza`, {
+      pizzaid,
+    });
+    alert('Pizza Deleted Successfully');
+    console.log(response);
+    window.location.reload();
+  } catch (error) {
+    alert('Something went wrong');
+    console.log(error);
+  }
+};
